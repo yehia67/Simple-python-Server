a4e0d7dc976a0ddf428e6ce9800d7bf86084eb80
yehia67 <yehiatarek67@gmail.com>
2019-04-25 15:57:24 +0200

    done creating server

--- server.py
+++ server.py
@@ -11,5 +11,17 @@ def main():
     try:
         message = connectionSocket.recv(1024)
         print(message,'::',message.split()[0],':',message.split()[1])
-
+        filename = message.split()[1]
+        print(filename,'||',filename[1:])
+        f = open(filename[1:])
+        outputdata = f.read()
+        print(outputdata)
+        connectionSocket.send('\nHTTP/1.1 200 OK\n\n')
+        connectionSocket.send(outputdata)
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i])
+            connectionSocket.close()
+    except IOError:
+         connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n')
+         connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n')  
       
