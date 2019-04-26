c7693f9b8895aaabc66311804be2a0292ab0136d
yehia67 <yehiatarek67@gmail.com>
2019-04-26 21:00:58 +0200

    add multiple server code

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 
 from socket import  *
+import threading
 def main():
-    serverPort=80
+    serverPort=9001
     serverSocket = socket(AF_INET, SOCK_STREAM)
     serverSocket.bind(('',serverPort))
     serverSocket.listen(1)
@@ -9,21 +10,23 @@ def main():
     while True:
           print ('Ready to serve...')
           connectionSocket, addr = serverSocket.accept()
-    try:
-        message = connectionSocket.recv(1024)
-        print(message,'::',message.split()[0],':',message.split()[1])
-        filename = message.split()[1]
-        print(filename,'||',filename[1:])
-        f = open(filename[1:])
-        outputdata = f.read()
-        print(outputdata)
-        connectionSocket.send('\nHTTP/1.1 200 OK\n\n')
-        connectionSocket.send(outputdata)
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i])
-            connectionSocket.close()
-    except IOError:
-         connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n')
-         connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n') 
+          try:
+              message = connectionSocket.recv(1024)
+              print(message,'::',message.split()[0],':',message.split()[1])
+              filename = message.split()[1]
+              print(filename,'||',filename[1:])
+              f = open(filename[1:])
+              outputdata = f.read()
+              print(outputdata)
+              connectionSocket.send('\nHTTP/1.1 200 OK\n\n')
+              connectionSocket.send(outputdata)
+              for i in range(0, len(outputdata)):
+                   connectionSocket.send(outputdata[i])
+                   connectionSocket.close()
+          except IOError:
+               connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n')
+               connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n') 
+               connectionSocket.close()
+     
+          serverSocket.close()     
 main()
-
